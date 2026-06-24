6c1edb163a710648342a4f5ee6626fb9e8e478ea
Remy E <remyscalendar@gmail.com>
2026-06-24 16:07:31 +1000

    Add files via upload

---
+++ src/client/java/com/remy/client/CrucibleClient.java
@@ -0,0 +1,10 @@
+package com.remy.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CrucibleClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/remy/client/CrucibleDataGenerator.java
@@ -0,0 +1,11 @@
+package com.remy.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CrucibleDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
